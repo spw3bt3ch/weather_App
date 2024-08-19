5a7bc4cfbd3210f3819d20755774b475d4cc3a9d
Samuel Ogunjimi <42714945+spw3bt3ch@users.noreply.github.com>
2024-08-19 15:23:20 +0100

    Update main.py
    modify location via the baselink

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from function import *
 import streamlit as st
 import requests
 
+st.title("SMI WEATHER APPLICATION")
+user_option = st.text_input("Enter Location", placeholder="Your Present location")
+
 # api = "http://api.weatherapi.com/v1/current.json?key=e09fb8315197455dac1115933231611&q=London&aqi=yes"
-api2 = "http://api.weatherapi.com/v1/current.json?key=e09fb8315197455dac1115933231611&q=Lagos, Nigeria&aqi=yes"
+api2 = f"http://api.weatherapi.com/v1/current.json?key=e09fb8315197455dac1115933231611&q={user_option}&aqi=yes"
 # getAPI = requests.get(api).json()
 getAPI = requests.get(api2).json()
-st.title("SMI WEATHER APPLICATION")
 
-user_option = st.text_input("Enter Location", placeholder="Your Present location")
 user_location = getAPI["location"]["name"]
 st.subheader("Weather Information")
 
